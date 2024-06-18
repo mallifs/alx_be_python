bc9b0ed857435c541dacad18a10d8f2297123993
mallifs <maillifs@icloud.com>
2024-06-18 14:24:04 +0100

    Basic-operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+subtraction = number1 - number2
+multiply = number1 * number2
+
+print("addition of", number1, "and", number2, "is", sum)
+print("subtraction of", number1, "and", number2, "is", subtraction)
+print("multiplication of", number1, "and", number2,"is", multiply)
