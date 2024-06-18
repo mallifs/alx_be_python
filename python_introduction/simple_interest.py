bf70faf09f058725f8a2a33a2628152d3a993457
mallifs <maillifs@icloud.com>
2024-06-18 15:24:47 +0100

    simple-i

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,14 @@
 principal = 1000
+
+ 
 rate = 0.05
+
+ 
 time = 3
 
+ 
 interest = principal * rate * time
 
-print("The simple interest is:", interest)
+# Print the calculated interest
+print(f"The simple interest is: {interest}")
 
