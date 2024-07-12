81e698f20f04423f64c48c1dc6825a817fc905df
mallifs <maillifs@icloud.com>
2024-07-12 11:30:51 +0100

    bank_account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+
+
+    def deposit(self, amount):
+        self._account_balance += amount
+
+
+    def withdraw(self, amount):
+        if self._account_balance >= amount:
+            self._account_balance -= amount
+            return True
+        else: 
+            return False
+
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self._account_balance:.2f}")
+
+
+                
+             
+
+
