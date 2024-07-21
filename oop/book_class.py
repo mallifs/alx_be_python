efabcfd7f1a4e2d6169ff93e6ff2bc4b09e98038
mallifs <maillifs@icloud.com>
2024-07-21 02:59:08 +0100

    oop

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,7 +9,7 @@ class Book:
     
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}, {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
 
     def __del__(self):
